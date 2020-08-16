9fc49bc7a34cea62adc5a9295ebf42129b36cf6b
adam <adambensee@gmail.com>
2020-08-15 22:32:29 -0400

    Updated LCG to prep for reading plot data from a stream

--- launch_control_gui.py
+++ launch_control_gui.py
@@ -5,6 +5,7 @@ from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import os
 import sys
+import random
 
 
 class MainWindow(QMainWindow):
@@ -12,13 +13,32 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         self.graphWidget = pg.PlotWidget()
+        self.graphWidget.setBackground('k')
         self.setCentralWidget(self.graphWidget)
 
-        hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
+        self.x = list(range(100))
+        self.y = [random.randint(0,100) for _ in range(100)]
 
         # plot data: x, y values
-        self.graphWidget.plot(hour, temperature)
+        self.my_line_ref = self.graphWidget.plot(self.x, self.y)
+
+        # for future reference, use a qthread that contains a socket connection,
+        # have socket connection run repeatedly (while true) checking for new data,
+        # once new data is received fire off qt signal which calls function
+        # update_plot_data() and feeding it the received data
+        self.timer = QTimer()
+        self.timer.setInterval(50)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+
+    def update_plot_data(self):
+        self.x = self.x[1:]
+        self.x.append(self.x[-1] + 1)
+
+        self.y = self.y[1:]
+        self.y.append(random.randint(0, 100))
+
+        self.my_line_ref.setData(self.x, self.y)
 
 
 def main():
