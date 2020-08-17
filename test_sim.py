7642599e7ced095fac2f2b78de132ea4017553e5
adam <adambensee@gmail.com>
2020-08-16 21:59:45 -0400

    Flushed out test sim further to meet use cases

--- test_sim.py
+++ test_sim.py
@@ -19,27 +19,24 @@ def bool_check(env, process_name, duration):
     print(process_name, env.now)
 
 
-def sub(env):
-    while env.now < 30:
+def sub(env, process_name, duration):
+    start_time = env.now
+    while env.now - start_time < duration:
         yield env.timeout(1)
-        print("fueling tank", env.now)
+        print(process_name, env.now)
 
 
-def parent(env):
-    sub_proc = yield start_delayed(env, sub(env), delay=20)
+def continuous_process(env, process_name, start_time, duration):
+    sub_proc = yield start_delayed(env, sub(env, process_name, duration), delay=start_time)
     ret = yield sub_proc
     return ret
 
 
 env = simpy.rt.RealtimeEnvironment(factor=1)
-# env.run(env.process(sub(env)))
 env.process(bool_check(env, "Check TVC", 5))
 env.process(bool_check(env, "Software Setup Check", 10))
-env.process(parent(env))
+env.process(continuous_process(env, "M1D Hydraulic Pressurization", 20, 20))
 env.process(bool_check(env, "M1D Check", 30))
-env.run(until=40)
-
-# env.process(fill_tank(env, 0.5))
-#
-# env.process(bool_check(env, "Final Setup Check", 10))
-# env.run(until=60)
+env.process(continuous_process(env, "LOX Fueling", 30, 20))
+env.process(bool_check(env, "RCS Check", 40))
+env.run(until=60)
