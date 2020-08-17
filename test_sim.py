c7ea97fbf2f796e55b1ec13e75cfbea57367359e
Adam <adambensee@gmail.com>
2020-08-16 21:47:00 -0400

    Created a working sim model!

--- test_sim.py
+++ test_sim.py
@@ -15,15 +15,31 @@ def fill_tank(env, tick):
 
 
 def bool_check(env, process_name, duration):
-    print(process_name, env.now)
     yield env.timeout(duration)
+    print(process_name, env.now)
+
+
+def sub(env):
+    while env.now < 30:
+        yield env.timeout(1)
+        print("fueling tank", env.now)
+
+
+def parent(env):
+    sub_proc = yield start_delayed(env, sub(env), delay=20)
+    ret = yield sub_proc
+    return ret
 
 
 env = simpy.rt.RealtimeEnvironment(factor=1)
-start_delayed(bool_check(env, "Check TVC", 5), delay=5)
+# env.run(env.process(sub(env)))
 env.process(bool_check(env, "Check TVC", 5))
 env.process(bool_check(env, "Software Setup Check", 10))
-env.process(bool_check(env, "M1D Check", 10))
-env.process(fill_tank(env, 0.5))
-env.process(bool_check(env, "Final Setup Check", 10))
-env.run(until=60)
+env.process(parent(env))
+env.process(bool_check(env, "M1D Check", 30))
+env.run(until=40)
+
+# env.process(fill_tank(env, 0.5))
+#
+# env.process(bool_check(env, "Final Setup Check", 10))
+# env.run(until=60)
